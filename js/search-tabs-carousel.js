8b3caa4e3c9e5eb41427a6db6e95f893b1abb50c
Abdo Farah <3bdo.farah@gmail.com>
2022-08-24 17:55:17 +0200

    make the carousel slider responsive

--- js/search-tabs-carousel.js
+++ js/search-tabs-carousel.js
@@ -14,8 +14,7 @@ let activeTabDescription = document.querySelector('#tab-description');
 let activeTabButton = document.querySelector('#tab-button');
 
 const getCourses = async () => {
-    for (let track in courses)
-    {
+    for (let track in courses) {
         const res = await fetch(`http://localhost:3000/${track}`);
         courses[track] = await res.json();
     }
@@ -36,15 +35,34 @@ form.addEventListener('submit', function (event) {
     buildCourses(key);
 });
 
+let coursesNav = document.querySelector('#courses-tabs');
+coursesNav.addEventListener('click', function (event) {
+    event.preventDefault();
+    //box.value = '';
+
+    let newActiveTab = event.target;
+    if (activeTab != newActiveTab) {
+        activeTab.classList.remove('active');
+        newActiveTab.classList.add('active');
+
+        let key = event.target.innerText.replace(' ', '-');
+        buildCourses(key);
+
+        activeTabHeader.innerText = courses[key]['header'];
+        activeTabDescription.innerText = courses[key]['description'];
+        activeTabButton.innerText = `Explore ${event.target.innerText}`;
+
+        activeTab = newActiveTab;
+    }
+});
+
 const box = document.querySelector('.search_box');
-function containsSearch (course)
-{
+function containsSearch(course) {
     if (course['title'].toLowerCase().includes(box.value.toLowerCase())
-     || course['headline'].toLowerCase().includes(box.value.toLowerCase()))
+        || course['headline'].toLowerCase().includes(box.value.toLowerCase()))
         return true;
 
-    for (let instructor of course['instructors'])
-    {
+    for (let instructor of course['instructors']) {
         if (instructor['name'].toLowerCase().includes(box.value.toLowerCase()))
             return true;
     }
@@ -52,20 +70,38 @@ function containsSearch (course)
 }
 
 
+
+function getNumberOfCardsPerSlide(width) {
+    if (width >= 1450)
+        return 5;
+    else if (width >= 1200)
+        return 4;
+    else if (width >= 992)
+        return 3;
+    else if (width >= 666)
+        return 2;
+    else
+        return 1;
+}
+
+
 const indicators = document.querySelector('.carousel-indicators');
 const slides = document.querySelector('.carousel-inner');
-function buildCourses (tab)
-{
+
+let cardsPerSlide = 0;
+function buildCourses(tab) {
     indicators.innerHTML = '';
     slides.innerHTML = '';
 
+    cardsPerSlide = getNumberOfCardsPerSlide(window.innerWidth);
+
     let temp = courses[tab]['courses'].filter(containsSearch);
-    for (let i=0; i<temp.length; i+=4) {
+    for (let i = 0; i < temp.length; i += cardsPerSlide) {
         const indicator = document.createElement('button');
         indicator.setAttribute('type', 'button');
         indicator.setAttribute('data-bs-target', '#carouselExampleIndicators');
-        indicator.setAttribute('data-bs-slide-to', `${i/4}`);
-        indicator.setAttribute('aria-label', `Slide ${(i/4)+1}`);
+        indicator.setAttribute('data-bs-slide-to', `${i / cardsPerSlide}`);
+        indicator.setAttribute('aria-label', `Slide ${(i / cardsPerSlide) + 1}`);
 
         if (i == 0) {
             indicator.setAttribute('class', 'active');
@@ -75,17 +111,17 @@ function buildCourses (tab)
         const carouselItem = document.createElement('div');
 
         carouselItem.classList.add('carousel-item');
-        if (i === 0){
+        if (i === 0) {
             carouselItem.classList.add('active');
         }
 
         const individualSlide = document.createElement('div');
-        individualSlide.classList.add('courses-inject', 'row', 'd-flex', 'justify-content-start');
+        individualSlide.classList.add('courses-inject', 'row', 'd-flex', 'flex-nowrsap', 'justify-content-start', 'm-auto');
 
-        for (let k=i; k<Math.min(i+4, temp.length); k++) {
+        for (let k = i; k < Math.min(i + cardsPerSlide, temp.length); k++) {
             let course = temp[k];
             individualSlide.innerHTML += `
-                <div class="mx-3">
+                <div class="mx-1">
                     <img src="${course['image']}" alt="course ${course['id']}">
                     <h3 class="title">${course['title']}</h3>
                     <h4>${course['instructors'][0]['name']}</h4>
@@ -101,27 +137,9 @@ function buildCourses (tab)
     }
 }
 
-
-
-let coursesNav = document.querySelector('#courses-tabs');
-coursesNav.addEventListener('click', function (event)
-{
-    event.preventDefault();
-    //box.value = '';
-
-    let newActiveTab = event.target;
-    if (activeTab != newActiveTab)
-    {
-        activeTab.classList.remove('active');
-        newActiveTab.classList.add('active');
-
-        let key = event.target.innerText.replace(' ', '-');
+addEventListener('resize', (event) => {
+    if (cardsPerSlide != getNumberOfCardsPerSlide(window.innerWidth)) {
+        let key = activeTab.innerText.replace(' ', '-');
         buildCourses(key);
-
-        activeTabHeader.innerText = courses[key]['header'];
-        activeTabDescription.innerText = courses[key]['description'];
-        activeTabButton.innerText = `Explore ${event.target.innerText}`;
-
-        activeTab = newActiveTab;
     }
 });
